0504e4379a23f94ce5c9a0375cb4c7a2cbd8ceea
TakExplores <tak.explores@gmail.com>
2022-03-23 21:16:26 +0800

    Finished 01, 02, 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let concatenatedString = '';
+    if (num >= 0) {
+        for (i = 0; i < num; i++) {
+            concatenatedString += string;
+        }
+        return concatenatedString;
+    } else {
+        return 'ERROR';
+    }
 };
+
+repeatString('hey', 5);
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringReversed = '';
+    for (let i = string.length -1; i >= 0; i--) {
+        stringReversed += string[i];
+    }
+    return stringReversed;
 };
+
+string = prompt('Enter any word or short sentence below');
+reverseString (string);
 
 // Do not edit below this line
 module.exports = reverseString;
